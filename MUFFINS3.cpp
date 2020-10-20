53353f30ebeeba5f4be439578e6000b73a851bd2
agnisain123 <agnisain123@gmail.com>
2020-10-20 21:49:32 +0530

    Uploaded solutions for C++

---
+++ MUFFINS3.cpp
@@ -0,0 +1,18 @@
+//Problem link: https://www.codechef.com/problems/MUFFINS3
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {   int a,b;
+        cin>>a;
+        b=(a/2)+1;
+        cout<<b<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
